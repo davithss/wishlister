938f511d0f822f4263e4cc6456fafd2c9ed81f69
Davi Thiesse <davithss@gmail.com>
2017-02-22 23:43:35 -0300

    Routes settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+      root 'home#index'
+      get 'wishlist', to: "home#wishlist"
+      get 'list', to: "home#add_list_item"
+      get 'list_info',  to: "home#list_info"
+      get 'delete_item', to: "home#delete_list_item"
+      get 'checkins', to: "home#checkins"
+      # get '/home/lists', to: "home#lists"
+      # get 'index', to: "home#index"
+
+  get 'find', to: 'find#index'
 end
