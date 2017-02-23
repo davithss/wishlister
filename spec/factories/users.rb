fdd2e611c95990b61fd96d1ff5b25e1e8ae14584
Davi Thiesse <davithss@gmail.com>
2017-02-22 23:50:59 -0300

    Rpecs

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    provider "foursquare"
+    uid "5443534"
+    name "Davi"
+    oauth_token "5645645644564"
+    oauth_expires_at "2017-02-15 11:46:02"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "Create in the future if necessary validates the model"
+end
