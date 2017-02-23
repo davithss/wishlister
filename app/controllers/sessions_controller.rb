8d02669b8c5fb4d19313cbae789b64a701eafdbe
Davi Thiesse <davithss@gmail.com>
2017-02-22 23:38:41 -0300

    Session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    session[:omniauth] = auth.except('extra')
+    user = User.sign_in_from_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to checkins_path, notice: "SIGNED IN"
+	end
+
+	def destroy
+		session[:user_id]  = nil
+		session[:omniauth] = nil
+		redirect_to root_url, notice: "SIGNED OUT"
+	end
+end
