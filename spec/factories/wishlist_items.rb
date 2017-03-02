9f9e1127caf6f118079b26e315942d519abfe62f
Davi Thiesse <davithss@gmail.com>
2017-03-02 20:53:36 -0300

    Wishlist item factory

---
+++ spec/factories/wishlist_items.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :wishlist_item do
+    name Faker::Name.name
+    address Faker::Address.street_address
+    photo_url 'http://photo.com'
+    wishlist_item
+  end
+end
