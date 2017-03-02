45f7f8b1f399c8ee1dd3597753628fe5acc475e6
Davi Thiesse <davithss@gmail.com>
2017-03-02 20:50:56 -0300

    Recent checkins Spec

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,35 +1,15 @@
 require 'rails_helper'
-require 'Foursquare2'
 
 RSpec.describe 'Home', type: :request do
-  # describe 'GET #index'
-  #   it "expect sign in" do
-  #     Foursquare2.configure do |config|
-  #       config.client_id = 'awesome'
-  #       config.client_secret = 'sauce'
-  #       config.api_version = 5551234
-  #       config.ssl = true
-  #     end
-  #     client = Foursquare2::Client.new
-  #     client.client_id.should == 'awesome'
-  #     client.client_secret.should == 'sauce'
-  #     client.api_version.should == 5551234
-  #     client.ssl.should == true
-  #   end
-  #
-  #
-  # describe 'GET #checkins'
-  #
-  #  context "Using foursquare API and bringing checkins" do
-  #   it "expect receive recent checkins from friends" do
-  #     expect do
-  #       stub_get("https://api.foursquare.com/v2/checkins/recent?oauth_token=#{@client.oauth_token}&limit=2", "checkins/friend_checkins.json")
-  #       checkins = client.recent_checkins(:limit => 2)
-  #       checkins.count.should == 2
-  #       checkins.first.venue.name.should == 'Buffalo Billiards'
-  #       checkins.last.user.firstName.should == 'David'
-  #     end
-  #   end
-  # end
 
+  describe 'GET #checkins' do
+    let(:user) { create(:user) }
+
+    it 'recent checkings from friends' do
+      client = stub_request(:get, "https://api.foursquare.com/v2/checkins/recent?oauth_token=user.oauth_token&v=20170215").
+      to_return(body: File.read('spec/support/api_responses/foursquare/recent_checkins/checkins.json'))
+      response = JSON.parse(client.response.body)
+      expect(response['response']['recent'][0]['user']['firstName']).to eq('Pardal')
+    end
+  end
 end
