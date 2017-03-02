027f62aef3e003966dfc4090e8d575864bed1850
Davi Thiesse <davithss@gmail.com>
2017-03-02 20:54:12 -0300

    Wishlist model Spec

--- spec/models/wishlist_spec.rb
+++ spec/models/wishlist_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Wishlist, type: :model do
   it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to have_many(:wishlist_items) }
+  it { is_expected.to accept_nested_attributes_for(:wishlist_items) }
 end
