01b60c9db714c6e22c0b4829e3027e0e756ad82c
Davi Thiesse <davithss@gmail.com>
2017-03-02 20:34:55 -0300

    Wishlist model was created

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,7 @@
 class Wishlist < ApplicationRecord
+  has_many :wishlist_items, dependent: :destroy
+  accepts_nested_attributes_for :wishlist_items, allow_destroy: true
+
   validates :name, uniqueness: true
   validates :name, presence: true
 end
