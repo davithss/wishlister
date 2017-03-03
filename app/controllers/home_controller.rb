ed3654a240f6c46bcefd30c1e4e102e80cac932f
Davi Thiesse <davithss@gmail.com>
2017-03-03 20:38:46 -0300

    Removed checkins method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def checkins
-    @profile = current_user.foursquare.user("self")
-    @user_photo = "#{@profile['photo']['prefix']}40x40#{@profile['photo']['suffix']}"
-    @checkins = current_user.foursquare.recent_checkins(limit:'10')
-  end
-
 end
