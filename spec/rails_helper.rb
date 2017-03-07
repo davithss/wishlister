3efa6500381fdc232b61f6f5ea8f00a1a458be18
Davi Thiesse <davithss@gmail.com>
2017-03-07 16:10:23 -0300

    rails_helper was refactored

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,20 +5,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'webmock/rspec'
 
-OmniAuth.config.test_mode = true
-omniauth_hash = {
-  'provider' => 'foursquare',
-  'uid' => '12345',
-  'info' => {
-    'name' => 'Chevchenko',
-    'email' => 'hi@chev.com',
-  },
-  'credentials' => {
-    'token' => '989u9h9h89',
-    'secret' => 'hahaeuh23828',
-  }
-}
-OmniAuth.config.add_mock(:foursquare, omniauth_hash)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -35,24 +22,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-
-  config.before(:each) do |example|
-    DatabaseCleaner.strategy = if example.metadata[:browser]
-      :truncation
-    else
-      :transaction
-    end
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
 end
