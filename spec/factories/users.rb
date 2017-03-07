395bc8da862b57990602d2fab1fb74d3153dd441
Davi Thiesse <davithss@gmail.com>
2017-03-07 16:00:33 -0300

    Changed oauth_token, uid and email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :user do
     provider "foursquare"
-    uid "5443534"
+    uid "15525570"
     name "Davi"
-    oauth_token "5645645644564"
+    oauth_token "LTKENYAWPD2F0CTEGG2NCQOFJJLHQDAJ4TFFZCOVMNRZB0NU"
+    email "davithss@gmail.com"
   end
 end
